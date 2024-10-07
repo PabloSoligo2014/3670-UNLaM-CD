928fea010454189abf5e895e3b443fe8b01113eb
psoligo@unlam.edu.ar <psoligo@unlam.edu.ar>
2024-10-07 16:00:09 -0300

    Clases pasadas

--- Code/CustomTransformers.py
+++ Code/CustomTransformers.py
@@ -3,6 +3,42 @@ import pandas as pd
 import numpy as np
 
 
+class ColumnScaler(BaseEstimator, TransformerMixin):
+    def __init__(self, scaler=None, columns=None):
+        super().__init__()
+        self.columns = columns
+        self.scaler = scaler
+    
+    def fit(self, X, y=None):
+        if self.columns is None:
+            self.columns = X.columns
+        self.scaler.fit(X[self.columns])
+        return self
+    
+    def get_feature_names_out(self, input_features=None):
+        return self.columns
+        
+    def transform(self, X, y=None):
+        Xc = X.copy()
+        Xc.loc[:,self.columns] = self.scaler.transform(Xc[self.columns])
+        return Xc
+
+class ColumnDropper(BaseEstimator, TransformerMixin):
+    def __init__(self, **kargs):
+        super().__init__()
+        self.columns = kargs["columns"]
+    
+    def fit(self, X, y=None):
+        self.input_columns = X.columns
+        return self
+    
+    def get_feature_names_out(self, input_features=None):
+        return [col for col in self.input_columns if col not in self.columns]
+        
+    def transform(self, X, y=None):
+        Xc = X.copy()
+        return Xc.drop(columns=self.columns, axis=1)
+
 class ColOutlierRemover(BaseEstimator, TransformerMixin):
     def __init__(self, percent=1.5, strategy="remove", columns=[]):
         super().__init__()
@@ -22,8 +58,8 @@ class ColOutlierRemover(BaseEstimator, TransformerMixin):
             upper_limit = Q3 + self.percent * iqr
             lower_limit = Q1 - self.percent * iqr
             if self.strategy=="remove":
-
-                Xc = Xc[(Xc[c] > lower_limit) & (Xc[c] < upper_limit)]
+                raise Exception("It's not working yet")
+                #Xc = Xc[(Xc[c] > lower_limit) & (Xc[c] < upper_limit)]
                 
             elif self.strategy=="limit":
                 Xc.loc[(Xc[c] < lower_limit), c] = lower_limit
@@ -34,3 +70,46 @@ class ColOutlierRemover(BaseEstimator, TransformerMixin):
                 raise Exception("Strategy not implemented yet")
             
         return Xc
+    
+class ColumnSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, columns=None):
+        super().__init__()
+        self.columns = columns
+    
+    def fit(self, X, y=None):
+        if self.columns is None:
+            self.columns = X.columns
+        return self
+    
+    def get_feature_names_out(self, input_features=None):
+        return self.columns
+        
+    def transform(self, X, y=None):
+        Xc = X.copy()
+        return Xc[self.columns]
+
+class CollinearityDropper(BaseEstimator, TransformerMixin):
+    def __init__(self, columns=None, min_coef=0.99):
+        super().__init__()
+        self.columns =  columns
+        self.min_coef = min_coef  
+        self.columns_to_drop = []    
+    
+    def fit(self, X, y=None):
+        if self.columns is None:
+            self.columns = X.select_dtypes(include=['number']).columns
+        
+        correlation_matrix = X[self.columns].corr()
+        fc = correlation_matrix.shape[1]
+        for i in range(fc):
+            for j in range(i+1, fc):
+                if abs(correlation_matrix.iloc[i, j]) > self.min_coef and correlation_matrix.columns[j] not in self.columns_to_drop:
+                    self.columns_to_drop.append(correlation_matrix.columns[j])
+        return self
+    
+    def get_feature_names_out(self, input_features=None):
+        return self.columns.drop(columns=self.columns_to_drop, axis=1)
+        
+    def transform(self, X, y=None):
+        Xc = X.copy()
+        return Xc.drop(columns=self.columns_to_drop, axis=1)
